0a2e21ec6c2e0f2e6ba22a05862e1881ec3a3232
javaage <hb_java@sina.com>
2016-05-23 20:33:28 +0800

    create new

---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MyrouteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
